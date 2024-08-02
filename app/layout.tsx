b69506ea66bf849c3399faf0aa84b527bd034b9c
shantoTechwoozy <168966485+shantoTechwoozy@users.noreply.github.com>
2024-08-02 15:22:04 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import Footer from "@/components/footer/page";
 import Header from "@/components/header/page";
+import CookieConsent from "@/components/Cookies/page";
+import { ClerkProvider, RedirectToSignIn } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} flex flex-col min-h-screen`}>
-        <Header />
-        <main className="flex-grow">{children}</main>
-        <Footer />
+        <ClerkProvider>
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+          <CookieConsent />
+        </ClerkProvider>
       </body>
     </html>
   );
